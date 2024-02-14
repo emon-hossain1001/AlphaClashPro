ca53313f908fa9633bf4a26cea117d3ae2fe676c
emon-hossain1001 <mrx00001001@gmail.com>
2024-02-14 12:10:20 +0600

    added more features

--- scripts/Alpha-clash.js
+++ scripts/Alpha-clash.js
@@ -1,3 +1,13 @@
+// function play(){
+//     // step-1: hide the home screen. to hide the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home');
+//     homeSection.classList.add('hidden');
+//     // show the playground
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+// }
+
 function play(){
-    console.log('play start now');
+    hideElementById('home');
+    showElementById('play-ground');
 }
