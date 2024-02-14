23420738252c6499c4aeab33bb2f91b6327938e6
emon-hossain1001 <mrx00001001@gmail.com>
2024-02-14 15:26:33 +0600

    added more features

--- scripts/Alpha-clash.js
+++ scripts/Alpha-clash.js
@@ -7,7 +7,22 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function continueGame(){
+    // step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet',alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    addBackgroundColorById(alphabet);
+}
+
 function play(){
     hideElementById('home');
     showElementById('play-ground');
+    continueGame();
 }
+
